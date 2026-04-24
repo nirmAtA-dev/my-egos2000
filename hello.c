31cc465e5b674e6392fb3a3d3529bc5156ec9ca5
nirmAtA-dev <nirmatad7d@gmail.com>
2026-04-25 00:02:34 +0530

    printf implementation

--- hello.c
+++ hello.c
@@ -11,6 +11,38 @@ void terminal_write(const char *str, int len) {
 #include <string.h>  // for strlen() and strcat()
 #include <stdarg.h>  // for va_start(), va_end(), va_arg() and va_copy()
 
+
+/* Implementing unsigned long long to ascii */
+void ulltoa(unsigned long long num, char * BUF, int radix){
+	int idx = 0;
+	int rem = 0;
+	while(num > 0){
+		rem = num % radix;
+		num = num / radix;
+		if(rem <= 9){
+			*(BUF + idx) = ('0' + rem);
+			idx++;
+		}
+		else{
+			*(BUF + idx) = ('A' + rem % 10);
+			idx++;
+		}
+	}
+	//reverse it
+	int start = 0;
+	int end = idx - 1;
+	while(start < end){
+		char temp = *(BUF + start);
+		*(BUF + start) = *(BUF + end) ;
+		*(BUF + end) = temp;
+		start++;
+		end--;
+	}
+	*(BUF + idx) = '\0';
+}
+		
+char* test = "Test";
+
 void format_to_str(char* out, const char* fmt, va_list args) {
     for(out[0] = 0; *fmt != '\0'; fmt++) {
         if (*fmt != '%') {
@@ -22,11 +54,31 @@ void format_to_str(char* out, const char* fmt, va_list args) {
             } else if (*fmt == 'd') {
                 itoa(va_arg(args, int), out + strlen(out), 10);	/* structure of itoa is (integer, char * buffer, radix) */
             } else if (*fmt == 'c') {
-		char arr[2];
-		arr[0] = va_arg(args, int);
-		arr[1] = '\0';
-		strcat(out, arr);
+		//terminal_write(test, 4);
+		char CBUF[2];
+		CBUF[0] = va_arg(args, int);
+		CBUF[1] = '\0';
+		strcat(out, CBUF);
+	    } else if (*fmt == 'x') {
+		//terminal_write(test, 4);
+		utoa(va_arg(args, int), out + strlen(out), 16);
+	    } else if (*fmt == 'u') {
+		//terminal_write(test, 4);
+		utoa(va_arg(args, unsigned int), out + strlen(out), 10);
+	    } else if (*fmt == 'p') {
+		//terminal_write(test, 4);
+		char ABUF[2];
+		ABUF[0] = '0';
+		ABUF[1] = 'x';
+		strcat(out, ABUF);
+		utoa(va_arg(args, unsigned int), out + strlen(out), 16);
+	    } else if (strncmp(fmt,"llu",3) == 0){
+		//terminal_write(test, 4);
+		fmt = fmt + 2;
+		ulltoa(va_arg(args, unsigned long long), out + strlen(out), 10);
+		
 	    }
+
         }
     }
 }
@@ -71,12 +123,10 @@ int main() {
      */
     printf("%s-%d is awesome!\n\r", "egos", 2000);
     printf("%c is character $\n\r", '$');
-    printf("%c is character 0", (char)48);
-    /*
-    printf("%x is integer 1234 in hexadecimal", 1234);
-    printf("%u is the maximum of unsigned int", (unsigned int)0xFFFFFFFF);
-    printf("%p is the hexadecimal address of the hello-world string", msg);
-    printf("%llu is the maximum of unsigned long long", 0xFFFFFFFFFFFFFFFFULL);
-    */
+    printf("%c is character 0\n\r", (char)48);
+    printf("%x is integer 1234 in hexadecimal\n\r", 1234);
+    printf("%u is the maximum of unsigned int\n\r", (unsigned int)0xFFFFFFFF);
+    printf("%p is the hexadecimal address of the hello-world string\n\r", msg);
+    printf("%llu is the maximum of unsigned long long\n\r", 0xFFFFFFFFFFFFFFFFULL);
     return 0;
 }
